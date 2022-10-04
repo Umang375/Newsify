86ecf1fb1df34c0774c4dddd3a9871e0c4b35584
Umang375 <ujain7315@gmail.com>
2022-10-04 21:16:43 +0530

    moving in between categories using react router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
+import { 
+  Routes, 
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize = {6} country = "in" category="science"/>
-      </div>
+          <Navbar/>
+          <Routes>
+            <Route path="general" element={<News key='general' pagesize = {6} country="in" category="general"/>} />
+            <Route path="business" element = {<News key="business" pagesize = {6} country="in" category="business"/>} />
+            <Route path="entertainment" element = {<News key="entertainment" pagesize = {6} country="in" category="entertainment"/>} />
+            <Route path="health" element = {<News key="health" pagesize = {6} country="in" category="health"/>} />
+            <Route path="science" element = {<News key="science" pagesize = {6} country="in" category="science"/>} />
+            <Route path="sports" element = {<News key="sports" pagesize = {6} country="in" category="sports"/>} />
+            <Route path="technology" element = {<News key="technology" pagesize = {6} country="in" category="technology"/>} />
+      </Routes>
+        </div>
     )
   }
 }
