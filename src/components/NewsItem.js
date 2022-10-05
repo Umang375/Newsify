c4506866e873f13476efa17212da3b3c78ecf996
Umang375 <ujain7315@gmail.com>
2022-10-05 22:04:54 +0530

    Added author, time and source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,13 @@ export class NewsItem extends Component {
     return (
       <div className = "my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src= {imageurl} className="card-img-top" alt="..."/>
+          <img src= {imageurl} className="card-img-top" alt='...'/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}<span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger " style={{left:'90%', zIndex: '1'}}> {source}
+            </span></h5>
             <p className="card-text">{description}...</p>
             <a href= {newsUrl} className="btn btn-sm btn btn-primary">Read More</a>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toUTCString()} </small></p>
           </div>
         </div>
       </div>
